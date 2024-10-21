05aa50f2679899166cb0cb4e7142ddd455e87656
Selaelo Serumula <serumula.selaelo.m@gmail.com>
2024-10-21 18:31:10 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const firstCurrencyEl = document.getElementById("currency-one");
+const secondCurrencyEl = document.getElementById("currency-two");
+const inputFieldEl = document.getElementById("amount-one");
+const exchangeResultsEl = document.getElementById("amount-two");
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+const apiKey = "ff97e1bcdcca273541fa0c17";
+
+const getCurrencies = async () => {
+  try {
+    let response = await fetch(
+      `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${firstCurrencyEl.value}`
+    );
+    let data = await response.json();
+
+    const rate = data.conversion_rates[secondCurrencyEl.value];
+
+    exchangeRateEl.innerText = `1 ${firstCurrencyEl.value} = ${rate} ${secondCurrencyEl.value}`;
+    exchangeResultsEl.value = (inputFieldEl.value * rate).toFixed(2);
+  } catch (error) {
+    exchangeRateEl.innerHTML = 'An error occured. Please try again later.';
+  }
+};
+
+getCurrencies();
+
+firstCurrencyEl.addEventListener("change", getCurrencies);
+secondCurrencyEl.addEventListener("change", getCurrencies);
+inputFieldEl.addEventListener("input", getCurrencies);
